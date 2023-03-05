eb7a44b62eae093a400540d881ab7300542acb09
Renzo Diaz <118644104+renzodiazjs@users.noreply.github.com>
2023-03-05 03:15:11 -0300

    add cmmit

---
+++ JS/function.js
@@ -0,0 +1,18 @@
+
+
+document.getElementById('icono_nombre').addEventListener('click', function () {
+    console.log("prueba_nombre")
+    document.getElementById("texto_imprimir").innerHTML = "Hola Soy Robert Garcia"
+
+})
+
+document.getElementById('icono_edad').addEventListener('click', function () {
+    document.getElementById("texto_imprimir").innerHTML = "29 años"
+
+})
+
+document.getElementById('icono_telefono').addEventListener('click', function () {
+    document.getElementById("texto_imprimir").innerHTML = "Tel: (415)338-8997"
+
+})
+
